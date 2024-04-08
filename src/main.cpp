43df3e67421f753b54bf963b8c05149af1d128ff
Alex Ryde <a.ryde@outlook.com>
2024-04-08 15:29:23 +1200

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <button.hpp>
+#include <stripLED.hpp>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -10,7 +12,6 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-std::atomic<uint8_t> state = 1;
 std::atomic<uint8_t> brightness = 255;
 
 const gpio_num_t BUTTON_NEXT = GPIO_NUM_10;
@@ -18,42 +19,19 @@ const gpio_num_t BUTTON_PREV = GPIO_NUM_11;
 const gpio_num_t BUTTON_PLUS = GPIO_NUM_46;
 const gpio_num_t BUTTON_MINUS = GPIO_NUM_9;
 
-void nextISR(void)
-{
-  ESP_LOGI("ISR", "Next State");
-  state = state + 1;
-}
-
-void prevISR(void)
-{
-  ESP_LOGI("ISR", "Prev State");
-  state = state - 1;
-}
-
-void increaseISR()
-{
-  ESP_LOGI("ISR", "Increase Brightness");
-  brightness += 51;
-}
-
-void decreaseISR()
-{
-  ESP_LOGI("ISR", "Decrease Brightness");
-  brightness -= 51;
-}
+button::button next(BUTTON_NEXT);
+button::button prev(BUTTON_PREV);
 
 BLECharacteristic *pCharacteristic;
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
+  next.setCallback(LED::toggleState);
+  prev.setCallback(LED::reverseToggleState);
+
   vTaskDelay(1000); //to stop boot lock
-
-  pinMode(BUTTON_NEXT, INPUT_PULLUP);
-  pinMode(BUTTON_PREV, INPUT_PULLUP);
-
-  attachInterrupt(BUTTON_NEXT, nextISR, FALLING);
-  attachInterrupt(BUTTON_PREV, prevISR, FALLING);
 
   Serial.println("Starting BLE work!");
 
@@ -61,12 +39,13 @@ void setup() {
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
+  
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pCharacteristic->setValue(std::to_string(state.load()));
+  pCharacteristic->setValue(std::to_string(static_cast<uint8_t>(LED::getState())));
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
@@ -81,8 +60,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly
 
-  pCharacteristic->setValue(std::to_string(state.load()));
-
+  pCharacteristic->setValue(std::to_string(LED::getState()));
 
   delay(2000);
 }
